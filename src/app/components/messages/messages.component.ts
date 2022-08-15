2ac5cca339eccaa24d6f1289dbae442f48421338
jenny <jennybecerra85@gmail.com>
2022-08-15 00:55:37 -0500

    cambios en message.componets

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl ,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ConexionService } from 'src/app/services/conexion.service';
 
 @Component({
@@ -8,77 +8,54 @@ import { ConexionService } from 'src/app/services/conexion.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-  form:FormGroup;
-  
-  /*formulario = new FormGroup(
-    {
-      Number: new FormControl(null,[Validators.required, Validators.maxLength(10), Validators.minLength(10)]),
-      Title: new FormControl(null),
-      Message: new FormControl(null), 
-      File: new FormControl(null) 
-    }
-  )*/
-  constructor(private fb: FormBuilder, private _ConexService: ConexionService) { 
+  form: FormGroup;
+  file: any;
+
+  constructor(private fb: FormBuilder, private _ConexService: ConexionService) {
     this.form = this.fb.group({
-      Number:['',[Validators.required, Validators.maxLength(10), Validators.minLength(10)]],
-      Title:[''],
-      Message:[''], 
-      File:[''] 
+      Number: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern("^[0-9]*$")]],
+      Title: [''],
+      Message: [''],
+      File: ['']
     })
 
-    
+    // console.log(this.form.get('File')?.value);
+
   }
-  
+
 
   ngOnInit(): void {
   }
 
-  EnviarDatos(){
-    console.log(this.form);
+  
 
-   /* const Data: any = {
-      Number: this.form.get('Number')?.value,
-      Title: this.form.get('Title')?.value,
-      Message: this.form.get('Message')?.value,
-     
+  EnviarDatos() {
+   
+
+    var formData = new FormData();
+    formData.set('Number', this.form.get('Number')?.value);
+    formData.set('Title', this.form.get('Title')?.value);
+    formData.set('Message', this.form.get('Message')?.value);
+    formData.set('File', this.file);
+
+    this._ConexService.postMensaje(formData).subscribe(
+      data => { alert("mensaje enviado") },
+      error => { alert("error al enviar el mensaje") });
+     // console.log("1");
+    
+  }
+
+  ObtenerFile(event: any) {
+ 
+    var archivo =event.target.files[0];
+    if (archivo.type == 'application/pdf' || archivo.type == 'application/x-zip-compressed' || archivo.type == 'image/jpeg' || archivo.type == 'image/png' || archivo.type == 'text/xml' ) {
+
+      this.file =archivo;
+
+    }else{
+      console.log( archivo.type);
     }
-    this._ConexService.postMensaje(Data).subscribe(data=>{alert("mensaje enviado")});
-    console.log(Data);*/
-   // this.form.reset()
-     var formData = new FormData();
-     formData.append('Number', this.form.get('Number')?.value);
-     formData.append('Title', this.form.get('Title')?.value );
-     formData.append('Message', this.form.get('Message')?.value);
-     formData.append('File', this.form.get('File')?.value);
-     this._ConexService.postMensaje(formData).subscribe(data=>{alert("mensaje enviado")});
-     //console.log(formData.get('Message'));
-     
-  }
+}
 
-  //file:any;
-
-  ObtenerFile(event:any){
-     
-    
-    //console.log(this.file);
-    
-      //console.log(this.file.type+' '+this.file.name);
-      if( event.target.files.lenght>0){
-        const file = event.target.files[0];
-
-        if(file.type =='application/pdf' || file.type =='application/x-zip-compressed' || file.type =='image/png' || file.type =='text/xml' || file.type =='image/jpge'){
-        this.form.patchValue({
-          File: file
-        })
-      }
-    
-      //hacermetodo para error de archivos
-      //console.log("error"+this.file.type + this.file.name);
-     
-    
-     
-  }
-
-
-  }
+  
 }
--- src/app/services/conexion.service.ts
+++ src/app/services/conexion.service.ts
@@ -5,12 +5,18 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+//peticiones http
+
+
 export class ConexionService {
-  myAppUrl ="https://localhost:44375/";
+  myAppUrl ="http://www.mensajes.somee.com/";
   myApiUrl ="CreateMessage";
   constructor(private http:HttpClient) { }
 
    postMensaje(data:any ): Observable<any>{
       return this.http.post(this.myAppUrl+this.myApiUrl,data);
    }
+
+   
 }
